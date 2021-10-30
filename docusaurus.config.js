233f78f95d7754bd4cec27da020a68810b8ab22a
MizuhoOkimoto <54873998+MizuhoOkimoto@users.noreply.github.com>
2021-10-30 00:16:16 -0400

    modify config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Dinosaurs_Sample',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://mizuhookimoto.github.io/docusaurus_sample',
+  baseUrl: '/docusaurus_sample',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'MizuhoOkimoto', // Usually your GitHub org/user name.
+  projectName: 'Dinosaurs_Sample', // Usually your repo name.
 
   presets: [
     [
